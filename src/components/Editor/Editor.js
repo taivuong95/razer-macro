32b1d2a01b732b6b407eb47b309364d541425726
TaiVuong <taivt95@gmail.com>
2019-05-31 01:20:47 +0700

    update editor UI

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -3,25 +3,50 @@ import PropTypes from "prop-types";
 import "./Editor.css";
 const Editor = props => {
   return (
-    <div className="widget widget-custom-for-editor" id="Editor">
-      <div className="head-editor">
-        <div className="check-item check-item-custom">
-          <input type="checkbox" />
-          <label  className="check-box">
-            <div className="check-text check-text-custom">
-              <li className="item-2">
-                <i className="icon time-2" />
-                <span>12.3s</span>
-              </li>
-            </div>
-          </label>
-        </div>
-        <div>
-          <i className="icon undo" />
-          <i className="icon redo" />
+    <>
+      <div className="widget widget-custom-head editor">
+        <div className="head-editor">
+          <div className="check-item check-item-custom">
+            <input type="checkbox" />
+            <label className="check-box">
+              <div className="check-text check-text-custom">
+                <li className="item-2">
+                  <i className="icon time-2" />
+                  <span>12.3s</span>
+                </li>
+              </div>
+            </label>
+          </div>
+          <div>
+            <i className="icon undo" />
+            <i className="icon redo" />
+          </div>
         </div>
       </div>
-    </div>
+
+      <div className="widget editor widget-custom-body">
+        <ul class="list-item list-item-editor">
+          <li class="item item-editor">
+            <input type="checkbox" />
+            <label className="check-box" />
+            <i class="icon icon-editor time" />
+            <span>Delay</span>
+          </li>
+          <li class="item item-editor">
+            <input type="checkbox" />
+            <label className="check-box" />
+            <i class="icon icon-editor keyboard" />
+            <span>Keyboard Function</span>
+          </li>
+          <li class="item item-editor">
+            <input type="checkbox" />
+            <label className="check-box" />
+            <i class="icon icon-editor keyboard" />
+            <span>Keyboard Function</span>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
--- src/components/ProfileBar/ProfileBar.js
+++ src/components/ProfileBar/ProfileBar.js
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { Context } from "../../store/Context";
-import useStore from '../../store/useStore';
+import useStore from "../../store/useStore";
 import StoreContext from "../../store/StoreContext";
-
 
 const ProfileBar = props => {
   //  const { state, dispatch } = useContext(Context);
 
   // const {state, dispatch} = useStore();
-  const {state, dispatch} = useContext(StoreContext);
+  const { state, dispatch } = useContext(StoreContext);
   return (
     <div className="profile-bar flex">
       <div className="loader" tooltip="Syncing Profiles" />
@@ -17,18 +16,11 @@ const ProfileBar = props => {
       <input type="text" name="profile" id="profileEdit" maxLength={25} />
       <div className="dropdown-area">
         <div id="profileDrop" className="s3-dropdown">
-          <div className="selected">macro 5</div>
+          <div className="selected">macro 1</div>
           <div className="icon expand" />
         </div>
         <div id="profileDropOpt" className="s3-options flex">
-          <div className="option">default macro</div>
-          <div className="option">macro 2</div>
-          <div className="option">macro 3</div>
-          <div className="option">macro 4</div>
-          <div className="option selected">macro 5</div>
-          <div className="option">macro 6</div>
-          <div className="option">macro 7</div>
-          <div className="option">macro 8</div>
+          <div className="option selected">macro 1</div>
         </div>
       </div>
       <div
@@ -36,7 +28,10 @@ const ProfileBar = props => {
         id="profileMenuToggle"
         onClick={() => dispatch({ type: "TOGGLE_EDIT" })}
       >
-        <div  className={state.profile.isEdit ? "profile-act show" : "profile-act"} id="profileMenu">
+        <div
+          className={state.profile.isEdit ? "profile-act show" : "profile-act"}
+          id="profileMenu"
+        >
           <div className="act action">add</div>
           <div className="act action">import</div>
           <div className="act divider" />
